606288e0ec8b012481e2a7b16f0c30bf808e4b46
Nemes1sX <kazstonk@gmail.com>
2022-12-20 20:04:43 +0200

    Added validation rules for input

--- SortingApp/Controllers/SortingController.cs
+++ SortingApp/Controllers/SortingController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SortingApp.Models.FormRequest.CustomRules;
 using SortingApp.Services;
 using System.ComponentModel.DataAnnotations;
-using SortingApp.Models.CustomRules;
 
 namespace SortingApp.Controllers
 {
